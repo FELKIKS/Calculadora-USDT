e3956a117d82d7073e0e522ce00e181abc3e956d
FELKIKS <92609299+FELKIKS@users.noreply.github.com>
2024-01-23 12:43:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function calculateResult(usdSpot, taxa) {
 function obterValorDoGtag() {
     // Lógica para obter o valor do gtag.js
     // Substitua por sua lógica real
-    return 4.9844; // Exemplo, substitua pelo valor real
+    return 4.9832; // Exemplo, substitua pelo valor real
 }
 
 function obterTaxaDaURL() {
